4948a3ca8f146575fb4c9b821ac7c0882f8e0cb6
dat02092k1 <ngodat02092001@gmail.com>
2023-09-14 09:32:13 +0700

    "store config"

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import {React, useEffect} from 'react'
 import RuleSet from '../RuleSet/RuleSet'
 import ListTitle from '../ListTitle/ListTitle'
 import ListVoting from '../ListVoting/ListVoting'
+import { useAppSelector, useAppDispatch } from '../../store/hook'
 
 function Main() {
+  const count = useAppSelector((state) => state.counters.value);
+  const dispatch = useAppDispatch();
+
+  useEffect((
+  ) => {
+    console.log('count', count);
+  }, [count]);
   return (
     <main className='relative z-10 lg:px-0'>
         <div className='mx-auto max-w-[930px]'>
