779cde5e95c73a2601acc0b55691ccb9df5cdc9d
dat02092k1 <ngodat02092001@gmail.com>
2023-09-16 16:32:11 +0700

    "persist state"

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,15 +1,30 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist'
+import thunk from 'redux-thunk';
 import counterReducer from './features/counter/counterSlice'
 import userReducer from './features/user/userSlice'
 import rapperReducer from './features/rapper/rapperSlice'
 
-export const store = configureStore({
-    reducer: {
-      counters: counterReducer,
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const rootReducer = combineReducers({
+  counters: counterReducer,
       user: userReducer,
       rappers: rapperReducer
-    },
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+    reducer: persistedReducer,
   })
+
+export const persistor = persistStore(store)
+
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
